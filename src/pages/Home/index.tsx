302cf847b85f9dec5976c744560da8f3da5b2673
NehaNaikdhure <nehanaikdhure@gmail.com>
2024-02-16 07:22:01 +0530

    og:image updated

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,7 +11,7 @@ const HomeLayout: React.FC = () => {
                 <title>Python Coding</title>
                 <meta property="og:title" content="Python Coding" />
                 <meta property="og:description" content="Python Data Science web based code editor for learning and practicing Python coding" />
-                <meta property="og:image" content="https://blog.logrocket.com/wp-content/uploads/2021/12/python-datetime-module.png" />
+                <meta property="og:image" content="https://e1.pxfuel.com/desktop-wallpaper/514/124/desktop-wallpaper-2048x2048-python-logo-ipad-air-backgrounds-and-python-code.jpg" />
                 <meta property="og:url" content="" />
                 <meta property="og:type" content="website" />
             </Helmet>
--- src/pages/IDE/index.tsx
+++ src/pages/IDE/index.tsx
@@ -44,7 +44,7 @@ function IDE({ }: Props) {
                 <title>Python Coding</title>
                 <meta property="og:title" content="Python Coding" />
                 <meta property="og:description" content="Python Data Science web based code editor for learning and practicing Python coding" />
-                <meta property="og:image" content="https://blog.logrocket.com/wp-content/uploads/2021/12/python-datetime-module.png" />
+                <meta property="og:image" content="https://e1.pxfuel.com/desktop-wallpaper/514/124/desktop-wallpaper-2048x2048-python-logo-ipad-air-backgrounds-and-python-code.jpg" />
                 <meta property="og:url" content="" />
                 <meta property="og:type" content="website" />
                 <script defer src="https://cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide.js"></script>
